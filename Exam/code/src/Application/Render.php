36dc87b2768b4a24bd4fa19da460855224236075
Admin <Admin@localhost>
2025-03-08 19:22:09 +0300

    Disable Cache Seminar 9

--- Exam/code/src/Application/Render.php
+++ Exam/code/src/Application/Render.php
@@ -14,7 +14,7 @@ class Render {
     public function __construct(){
         $this->loader = new FilesystemLoader($_SERVER['DOCUMENT_ROOT'] . '/../' . $this->viewFolder);
         $this->environment = new Environment($this->loader, [
-            'cache' => $_SERVER['DOCUMENT_ROOT'].'/../cache/',
+            //'cache' => $_SERVER['DOCUMENT_ROOT'].'/../cache/',
         ]);
     }
 
